e661d5cd8352076b35804623f14e015b0fd09391
LaiQiang Ding <lding@splunk.com>
2017-03-24 11:17:01 +0800

    remove the meaningless code

--- freezedata/freezedata.py
+++ freezedata/freezedata.py
@@ -60,7 +60,6 @@ def freeze_data(obj, allow=frozenset()):
     if isinstance(obj, types.ModuleType):
         return freeze_class_or_istance_or_module(obj, allow=allow)
     else:
-        print(123)
         return freeze_data_inner(obj, allow=allow)
 
 
